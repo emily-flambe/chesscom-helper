4cc4042709d0cd657e53a5b7949a649318add3c5
Flambé <emilycogsdill@gmail.com>
2025-01-20 13:11:14 -0700

    add command get_chesscom_user

--- chesscom_helper/notify_me/admin.py
+++ chesscom_helper/notify_me/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from notify_me.models import Dummy
-
+from notify_me.models import Dummy, User
 
 class DummyAdmin(admin.ModelAdmin):
     pass
 
+class UserAdmin(admin.ModelAdmin):
+    pass
+
 
 admin.site.register(Dummy, DummyAdmin)
+admin.site.register(User, UserAdmin)
--- chesscom_helper/notify_me/models.py
+++ chesscom_helper/notify_me/models.py
@@ -14,3 +14,22 @@ class Dummy(models.Model):
 
     def __str__(self):
         return f"{self.id} - {self.created_at}"
+    
+class User(models.Model):
+    player_id = models.BigIntegerField(primary_key=True, unique=True)  # Primary key
+    url = models.URLField()
+    name = models.CharField(max_length=255, null=True, blank=True)
+    username = models.CharField(max_length=150, unique=True)
+    followers = models.IntegerField(default=0)
+    country = models.URLField(null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    last_online = models.BigIntegerField()
+    joined = models.BigIntegerField()
+    status = models.CharField(max_length=50)
+    is_streamer = models.BooleanField(default=False)
+    verified = models.BooleanField(default=False)
+    league = models.CharField(max_length=50, null=True, blank=True)
+    streaming_platforms = models.JSONField(default=list, blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.name})"
