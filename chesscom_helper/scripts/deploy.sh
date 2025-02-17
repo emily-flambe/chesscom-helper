07e3b316e936715cb654c467342ce6397c165ac1
Flambé <emilycogsdill@gmail.com>
2025-02-16 20:15:04 -0700

    reorganize ec2 scripts

---
+++ chesscom_helper/scripts/deploy.sh
@@ -0,0 +1,5 @@
+docker-compose restart
+docker-compose exec web python manage.py migrate
+docker-compose exec web python manage.py collectstatic --noinput
+docker-compose exec web bash -c "npm run build"
+docker-compose exec web bash -c "npm run start"
