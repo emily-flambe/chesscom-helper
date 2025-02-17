52dce4469464aaaacadabd9b8684b31a716dd14e
Flambé <emilycogsdill@gmail.com>
2025-02-16 20:44:24 -0700

    try a nohup strat

--- deploy_scripts/deploy-dev.sh
+++ deploy_scripts/deploy-dev.sh
@@ -2,4 +2,4 @@ docker-compose restart
 docker-compose exec web python ../manage.py migrate
 docker-compose exec web python ../manage.py collectstatic --noinput
 docker-compose exec web bash -c "npm run build"
-docker-compose exec web bash -c "npm run dev" &
+docker-compose exec -T web bash -c "nohup npm run dev > /dev/null 2>&1 &"
