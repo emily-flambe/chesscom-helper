7755238682b3d389d6bd06b149a1bb4ced5fc9b0
Flambé <emilycogsdill@gmail.com>
2025-03-16 12:06:42 -0600

    Update vite.config.js

--- chesscom_helper/frontend/vite.config.js
+++ chesscom_helper/frontend/vite.config.js
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  build: {
+    manifest: true,
+    outDir: "dist",
+  },
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
-    allowedHosts: ['chesscomhelper.com', 'www.chesscomhelper.com', 'localhost', '127.0.0.1', 'dev.chesscomhelper.com', 'chesscomhelper.emilyflam.be'],
+    allowedHosts: true,
     watch: {
       usePolling: true,
     },
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://0.0.0.0:8000',
         changeOrigin: true,
         secure: false,
       }
