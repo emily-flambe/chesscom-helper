d0c210d0c46f8c69c9475c6734aae329416a77b4
Flambé <emilycogsdill@gmail.com>
2025-03-17 09:19:17 -0600

    Update vite.config.js

--- chesscom_helper/frontend/vite.config.js
+++ chesscom_helper/frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
-    allowedHosts: true,
+    allowedHosts: ['chesscomhelper.emilyflam.be'],
     watch: {
       usePolling: true,
     },
